e6aa980fbf0e3103a461bf99cb9fe4219f0a5d61
nju33 <nju33.ki@gmail.com>
2019-08-30 17:39:03 +0900

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 
-set -xe
+replace () {
+  for f in *.tsx *.ts
+  do
+    file_name=$1
+    file_name_in_lower_camel_case="$(F="$file_name" node -p "process.env.F.replace(/-([a-z])/g, (_, p) => p.toUpperCase())")"
 
-if test -z $1
+    sed -i "s/COMPONENT_NAME/$file_name_in_lower_camel_case/g" "$f"
+    sed -i "s/COMPONENT_FILE_NAME/$file_name/g" "$f"
+
+    if [ "$f" != "index.ts" ]; then
+      mv "$f" "$(echo "$f" | sed "s/COMPONENT_FILE_NAME/$file_name/")"
+    fi
+  done
+}
+
+post_process () {
+  rm setup.sh
+}
+
+if test -z "$1"
 then
   echo コンポート名（ケバブケース）を指定してください
   echo './setup.sh component-name'
   exit 1
 fi
 
-for f in *.ts
-do
-  local file_name=$1
-  local file_name_in_lower_camel_case=`F="$file_name" node -p "process.env.F.replace(/-([a-z])/g, (_, p) => p.toUpperCase())"`
+set -xe
 
-  sed -i "s/COMPONENT_NAME/$file_name_in_lower_camel_case/g" $f
-  sed -i "s/COMPONENT_FILE_NAME/$file_name/g" $f
-  mv $f `echo "$f" | sed "s/COMPONENT_FILE_NAME/$file_name/"`
-done
-
-rm setup.sh
+replace "$1"
+# post_process

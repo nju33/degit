56bde441ca754af497ea0bf3a37e491afcafdbf4
nju33 <nju33.ki@gmail.com>
2019-09-18 03:30:01 +0900

    fix(setup.sh): add new placeholder DUCK_NAME_IN_PASCAL

--- DUCK_FILE_NAME.hooks.ts
+++ DUCK_FILE_NAME.hooks.ts
@@ -1,11 +1,11 @@
 import * as operators from './DUCK_FILE_NAME.operators';
 import * as selectors from './DUCK_FILE_NAME.selectors';
 
-export const useDUCK_NAME = ({
+export const useDUCK_NAME_IN_PASCAL = ({
   dispatch
 }: {
   dispatch: import('redux').Dispatch;
 }) => ({
   ...selectors,
-  initialize: operators.initializeOperator(dispatch),
+  initialize: operators.initializeOperator({dispatch}),
 });
--- setup.sh
+++ setup.sh
@@ -4,14 +4,17 @@ replace () {
   for f in *.ts
   do
     file_name=$1
+    file_name_in_pascal="$(F="$file_name" node -p "process.env.F.replace(/^(.)/, (_, p) => p.toUpperCase()).replace(/-([a-z])/g, (_, p) => p.toUpperCase())")"
     file_name_in_lower_camel_case="$(F="$file_name" node -p "process.env.F.replace(/-([a-z])/g, (_, p) => p.toUpperCase())")"
 
-    if which gsed; then
-      gsed -i "s/DUCK_NAME/$file_name_in_lower_camel_case/g" "$f"
+    if command -v gsed; then
+      gsed -i "s/DUCK_NAME_IN_PASCAL/$file_name_in_pascal/g" "$f"
       gsed -i "s/DUCK_FILE_NAME/$file_name/g" "$f"
+      gsed -i "s/DUCK_NAME/$file_name_in_lower_camel_case/g" "$f"
     else
-      sed -i "s/DUCK_NAME/$file_name_in_lower_camel_case/g" "$f"
+      sed -i "s/DUCK_NAME_IN_PASCAL/$file_name_in_pascal/g" "$f"
       sed -i "s/DUCK_FILE_NAME/$file_name/g" "$f"
+      sed -i "s/DUCK_NAME/$file_name_in_lower_camel_case/g" "$f"
     fi
 
     if [ "$f" != "index.ts" ]; then
